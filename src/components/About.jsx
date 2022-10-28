f4db51117976443aa9d63e21f77526ed9494298e
Luis <luisf91mv@gmail.com>
2022-10-28 08:42:08 +0100

    Added my project

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className="about light">
+      <h1 className="about-heading heading">About</h1>
+      <p className="about-text text">
+        Im a fullstack developer base in Spain. I work with javascript/python
+        nodejs mongodb react and figma. Im really interested in IA and y have
+        some project with stablefusion.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Interests.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Interests() {
+  return (
+    <div className="interests light">
+      <h1 className="interests-heading heading">Interests</h1>
+      <p className="interests-text text">
+        I love rock music, calistenics workout, and documentaries.
+      </p>
+    </div>
+  )
+}
+
+export default Interests
